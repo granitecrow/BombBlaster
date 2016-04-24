c848e501e94f3cba9b164aaeb858490e31d5cd31
granitecrow <granite.crow@gmail.com>
2016-04-24 14:35:00 -0400

    Pathfinding Test
    Added scene to visualize pathfinding AI test

--- Assets/Scripts/AI/PathfindingTest.cs
+++ Assets/Scripts/AI/PathfindingTest.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
 public class PathfindingTest : MonoBehaviour {
+
+    public GameObject mapGroup;
 
 	// Use this for initialization
 	void Start () {
@@ -9,8 +13,8 @@ public class PathfindingTest : MonoBehaviour {
         {
             {0, 1, 0, 0, 0},
             {0, 1, 0, 0, 0},
-            {0, 1, 0, 0, 0},
-            {0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 1, 1, 0, 0},
             {0, 0, 0, 0, 0}
         };
 
@@ -25,8 +29,30 @@ public class PathfindingTest : MonoBehaviour {
         }
 
         print("Search done. Path length " + search.path.Count + " iterations " + search.iterations);
+
+        ResetMapGroup(graph);
+
+        foreach (var node in search.path)
+        {
+            GetImage(node.label).color = Color.red;
+        }
 	}
+
+    Image GetImage(string label)
+    {
+        var id = Int32.Parse(label);
+        var go = mapGroup.transform.GetChild(id).gameObject;
+        return go.GetComponent<Image>();
+    }
 	
+    void ResetMapGroup(Graph graph)
+    {
+        foreach (var node in graph.nodes)
+        {
+            GetImage(node.label).color = node.adjacent.Count == 0 ? Color.white : Color.grey;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
